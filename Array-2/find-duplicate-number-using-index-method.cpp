388d7df344c122d6cd544142e7b1df46b1e79c21
durgaprasad-sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-10 01:54:37 +0530

    Create find-duplicate-number-using-index-method.cpp

---
+++ Array-2/find-duplicate-number-using-index-method.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        //2 3 1 2 3
+        //2%5 = 1;
+        //2nd index i.e 1+n = 6
+        for(int i=0; i<n; i++){
+            int index = nums[i]%n;
+            nums[index] = nums[index]+n;
+        }
+        //2 8 11 12 3
+        //if arr[i]>=2 then the index is the duplicate number
+        for(int i=0; i<n; i++){
+            if(nums[i]/n>=2){
+                ans.push_back(i);
+            }
+        }
+        
+        if(ans.size()==0){ 
+            return {-1};
+        }
+        return ans[0];
+    }
+};
