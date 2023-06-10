1c1f17740ea7faada50ce12877f4bde1ad557b34
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-10 15:55:58 +0530

    Create repeat-and-missing-num-array.cpp

---
+++ Array-2/repeat-and-missing-num-array.cpp
@@ -0,0 +1,18 @@
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    long long n = A.size();
+    long long sn = (n* (n+1))/2;
+    long long s2n = (n* (n+1) * (2*n+1))/6;
+    long long s=0, s2=0;
+    for(int i=0; i<n; i++){
+        s+=A[i];
+        s2+= (long long)A[i] * (long long)A[i];
+    }
+    long long val1= s-sn;
+    long long val2= s2-s2n;
+    //finding value 2(i.e solving xsquare- ysquare = val2);
+    val2 = val2/val1;
+    long long x = (val1+val2)/2;
+    long long y = x-val1;
+    
+    return {(int)x, (int)y};
+}
