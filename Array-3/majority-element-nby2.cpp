44b50183b1a78673f5cd62fe008f2e5e5d648436
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-11 11:02:58 +0530

    Update majority-element-nby2.cpp

--- Array-3/majority-element-nby2.cpp
+++ Array-3/majority-element-nby2.cpp
@@ -1,16 +1,43 @@
-//map
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int>mpp;
-        int n= nums.size();
+        // map<int,int>mpp;
+        // int n= nums.size();
+        // for(int i=0; i<n; i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second> (n/2)){
+        //         return it.first;
+        //     }
+        // }return -1;
+
+        //Moore's voting algorithm
+        int n = nums.size();
+        int candidate;
+        int votes=0;
+        //here we'll find the candidate key i.e the max
         for(int i=0; i<n; i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp){
-            if(it.second> (n/2)){
-                return it.first;
+            if(votes==0){
+                votes = 1;
+                candidate = nums[i];
+            }else{
+                if(nums[i]==candidate){
+                    votes++;
+                }else{
+                    votes--;
+                }
             }
-        }return -1;
+        }
+        //check the count of the candidate key
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==candidate){
+                count++;
+            }
+        }
+        //check if candidate's count is greater than n/2
+        if(count> (n/2)) return candidate;
+        return -1;
     }
 };
