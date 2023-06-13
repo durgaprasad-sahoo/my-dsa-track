bb3a9ad89c8d7b9827b7059247ba38a03b191024
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-13 20:53:42 +0530

    Create longest-subarray-with-sum-k.cpp

---
+++ Array4/longest-subarray-with-sum-k.cpp
@@ -0,0 +1,25 @@
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        //15 -2 2 -8 1 7 10 23
+        int K = 0;
+        int sum = 0;
+        int maxLength = 0;
+        map<int, int>mpp;
+        for(int i=0; i<n; i++){
+            
+            sum+= A[i];
+            if(sum==K) maxLength = max(maxLength, i+1);
+            int rem = sum-K;
+            if(mpp.find(rem)!=mpp.end()){
+                int len = i-mpp[rem];
+                maxLength= max(maxLength, len );
+            }
+            if(mpp.find(sum)==mpp.end()){
+                mpp[sum]=i;
+            }
+        }
+        return maxLength;
+    }
+};
