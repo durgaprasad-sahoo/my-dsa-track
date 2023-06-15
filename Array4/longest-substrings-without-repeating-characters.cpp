aafa39433612a13751604cd03c71841e11dfedfe
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-15 23:51:54 +0530

    Update longest-substrings-without-repeating-characters.cpp

--- Array4/longest-substrings-without-repeating-characters.cpp
+++ Array4/longest-substrings-without-repeating-characters.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int>mpp (256,-1);
-        int l = 0;
-        int r = 0;
+        int l = 0, r = 0;
         int len = 0;
         //abcaabcdba
         while(r<s.size()){
