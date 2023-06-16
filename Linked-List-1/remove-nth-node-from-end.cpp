2f787d6155df145456b1d640d024115cd2b0ad2e
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-17 00:53:31 +0530

    Update remove-nth-node-from-end.cpp

--- Linked-List-1/remove-nth-node-from-end.cpp
+++ Linked-List-1/remove-nth-node-from-end.cpp
@@ -38,3 +38,27 @@ public:
         return head;
     }
 };
+//optimal approach
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+        if (fast==NULL) {
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
+        }
+        
+        while (fast->next != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        return head;
+    }
+};
