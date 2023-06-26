70647f451ae4ae41c51bcad5a0e572de9f452d40
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-26 16:42:48 +0530

    Create print-all-permutations.cpp

---
+++ recursion-and-backtrack/print-all-permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rec(int index, vector<int>&nums, vector<vector<int>>&ans){
+        if(index==nums.size()){
+            ans.push_back(nums); 
+            return;
+        }
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[i], nums[index]);
+            rec(index+1, nums, ans);
+            swap(nums[i], nums[index]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        rec(0, nums, ans);
+        return ans;
+    }
+};
