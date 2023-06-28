0dc79805459bf41c439e634f8715fd59192aaa4f
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-29 02:46:27 +0530

    Update search-in-a-rotated-sorted-arr.cpp

--- binary-search/search-in-a-rotated-sorted-arr.cpp
+++ binary-search/search-in-a-rotated-sorted-arr.cpp
@@ -5,18 +5,22 @@ public:
         // ex1: 4 5 6 7 0 1 2;
         // ex2: 5 6 0 1 2 3 4;
         // ex3: 6 7 1 2 3 4 5;
+
+        //always search  in the sorted order first
         int low = 0, high = nums.size()-1;
         while(low<=high){
             int mid = (low+high)/2;
             if(nums[mid]==target) return mid;
-            //right sorted
+            // left sorted array
             if(nums[low]<=nums[mid]){
                 if(target>= nums[low] && target<=nums[mid]){
                     high = mid-1;
                 }else low = mid+1;
-            }else{
-                //if(nums[low]>nums[mid])
-                if(target>=nums[mid] && target <=nums[high]){
+            }
+            //right sorted array
+            else if(nums[low]>nums[mid]){
+                //
+                if(target >= nums[mid]&& target<=nums[high]){
                     low = mid+1;
                 }else high = mid-1;
             }
@@ -24,3 +28,6 @@ public:
         return -1;
     }
 };
+
+
+
