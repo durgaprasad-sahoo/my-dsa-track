03c0b9a14e4f5da09f971146174f49135ef513be
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-29 02:42:40 +0530

    Create search-in-a-rotated-sorted-arr.cpp

---
+++ binary-search/search-in-a-rotated-sorted-arr.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        //ex0:  1 2 3 4 5 6 7;
+        // ex1: 4 5 6 7 0 1 2;
+        // ex2: 5 6 0 1 2 3 4;
+        // ex3: 6 7 1 2 3 4 5;
+        int low = 0, high = nums.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target) return mid;
+            //right sorted
+            if(nums[low]<=nums[mid]){
+                if(target>= nums[low] && target<=nums[mid]){
+                    high = mid-1;
+                }else low = mid+1;
+            }else{
+                //if(nums[low]>nums[mid])
+                if(target>=nums[mid] && target <=nums[high]){
+                    low = mid+1;
+                }else high = mid-1;
+            }
+        }
+        return -1;
+    }
+};
