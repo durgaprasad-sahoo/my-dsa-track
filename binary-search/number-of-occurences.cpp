d071866e5026c8378bd32cafdc4e7a321833728c
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-29 00:36:16 +0530

    Update number-of-occurences.cpp

--- binary-search/number-of-occurences.cpp
+++ binary-search/number-of-occurences.cpp
@@ -4,8 +4,10 @@ public:
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
 	    int lb = lower_bound(arr, arr+n, x)-arr;
-	    int last_occurence = upper_bound(arr, arr+n, x)-arr-1;
+	    int ub = upper_bound(arr, arr+n, x)-arr;
 	    if(lb==n || arr[lb]!=x) return 0;
-	    else return last_occurence-lb+1;
+	    else return ub-lb;
 	}
 };
+
+
