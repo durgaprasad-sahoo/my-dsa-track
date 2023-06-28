34e16732cd1ef59e4f89fe6d0de74b69824c331e
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-06-29 00:34:46 +0530

    Create number-of-occurences.cpp

---
+++ binary-search/number-of-occurences.cpp
@@ -0,0 +1,11 @@
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    int lb = lower_bound(arr, arr+n, x)-arr;
+	    int last_occurence = upper_bound(arr, arr+n, x)-arr-1;
+	    if(lb==n || arr[lb]!=x) return 0;
+	    else return last_occurence-lb+1;
+	}
+};
