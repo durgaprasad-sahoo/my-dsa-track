f3d26005f42c8171190da2839f2040127b82391a
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-07-09 23:32:39 +0530

    Create check-valid-palindrome.cpp

---
+++ strings/check-valid-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        // A man, a plan, a canal: Panama
+        int left = 0;
+        int right = s.size()-1;
+        while(left<=right){
+            if(!isalnum(s[left])){left++; continue;}
+            if(!isalnum(s[right])){right--; continue;}
+            if(tolower(s[left])!= tolower(s[right])) return false;
+            left++;
+            right--;
+        }return true;
+    }
+};
