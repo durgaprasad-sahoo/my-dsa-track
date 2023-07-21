240145499e0380c803a40be9758749ddcac7718c
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-07-21 22:50:45 +0530

    Create min-stack(using single stack).cpp

---
+++ Stack-and-queues/min-stack(using single stack).cpp
@@ -0,0 +1,46 @@
+class MinStack {
+private:
+    stack<long long> st;
+    long long mini;
+
+public:
+    MinStack() {
+        mini = INT_MAX;
+    }
+
+    void push(int value) {
+        long long val = value;
+        if (st.empty()) {
+            mini = val;
+            st.push(val);
+        } else {
+            if (val < mini) {
+                st.push(2 * val * 1LL - mini);
+                mini = val;
+            } else {
+                st.push(val);
+                // as val is not less than mini, so no need to update mini
+            }
+        }
+    }
+
+    void pop() {
+        if (st.empty()) return;
+        long long el = st.top();
+        st.pop();
+        if (el < mini) {
+            mini = 2 * mini - el;
+        }
+    }
+
+    int top() {
+        if (st.empty()) return -1;
+        long long el = st.top();
+        if (el < mini) return mini;
+        else return el;
+    }
+
+    int getMin() {
+        return mini;
+    }
+};
