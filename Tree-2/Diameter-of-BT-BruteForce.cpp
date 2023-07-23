451161917d57fd79a10c9a31ed666db2cf96e121
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-07-24 00:42:26 +0530

    Create Diameter-of-BT-BruteForce.cpp

---
+++ Tree-2/Diameter-of-BT-BruteForce.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int maxDiameter=0;
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+        int leftHeight= height(root->left);
+        int rightHeight=height(root->right);
+        maxDiameter= max(maxDiameter, leftHeight+rightHeight);
+        return max(leftHeight, rightHeight)+1;
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return maxDiameter;
+    }
+};
