39a942ace4d3b801292150bdc04f0b096ffb47ce
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-07-24 03:03:28 +0530

    Update mirror-the-BT.cpp

--- Tree-3/mirror-the-BT.cpp
+++ Tree-3/mirror-the-BT.cpp
@@ -1,3 +1,4 @@
+//GFG
 /* A binary tree node has data, pointer to left child
    and a pointer to right child /
 struct Node
@@ -20,5 +21,27 @@ class Solution {
         swap(root->left, root->right);
         mirror(root->left);
         mirror(root->right);
+    }
+};
+//LeetCode
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL) return NULL;
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
     }
 };
