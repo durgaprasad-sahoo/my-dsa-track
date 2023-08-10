330b1d9e16734950ff2149c365cdc6ce4a30c64f
Durgaprasad Sahoo <105366468+durgaprasad-sahoo@users.noreply.github.com>
2023-08-10 17:21:09 +0530

    Update merge-two-sorted-ll(optimal).cpp

--- Linked-List-1/merge-two-sorted-ll(optimal).cpp
+++ Linked-List-1/merge-two-sorted-ll(optimal).cpp
@@ -11,16 +11,16 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1) return list2;
-        if(!list2) return list1;
-        if(list1->val > list2->val) swap(list1, list2); //the minimum num should start from list1
+        if (!list1) return list2;
+        if (!list2) return list1;
+        if(list1->val > list2->val) swap(list1, list2);
         ListNode* ansNode = list1;
         while(list1 and list2){
-            ListNode* temp = NULL;
+            ListNode* lastSmall = NULL;
             while(list1 and list1->val <= list2->val){
-                temp = list1;
-                list1= list1->next;
-            }temp->next = list2;
+                lastSmall = list1;
+                list1 = list1->next;
+            }lastSmall->next = list2;
             swap(list1, list2);
         }return ansNode;
     }
